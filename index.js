d647f434a261179770aa3bcfb72c893b717fb095
neeraj7271 <neeraj8829sini@gmail.com>
2024-01-24 11:40:38 +0530

    addeed vercel configuration

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Define a sample endpoint
-app.get('/api/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: 'Hello, this is your API!' });
 });
 
