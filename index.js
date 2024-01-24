a1c42e3aa14619b9fa456e1e05db6c0165955418
neeraj7271 <neeraj8829sini@gmail.com>
2024-01-24 11:30:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// Define a sample endpoint
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, this is your API!' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
